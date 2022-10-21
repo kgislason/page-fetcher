a9124811fd72ad6fcd470d3c561eed4341b95182
Kristy Gislason <kristygislason@gmail.com>
2022-10-20 21:23:52 -0700

    Logged the message of files dowloaded with ### bytes. Updated README, removed the downloaded file from git

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Page Downloader
 
+App that takes 2 command line arguments: a URL and a local file path. The app will download the resource at the URL to the local path on your machine. Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
+
+## Installation
+
+`npm install`
+
+`node fetcher.js <URL> <FILEPATH>`
+
+I.e. Example node fetcher.js http://www.example.edu/ ./index.html
--- fetcher.js
+++ fetcher.js
@@ -1,38 +1,41 @@
+/**
+ * Page Fetcher
+ * 
+ * 
+// Downloaded and saved 3261 bytes to ./index.html
+ */
 const fs = require("fs");
 const request = require('request');
 
-// It should take two command line arguments:
-
-// a URL
-// a local file path
+// Accept command line arguments
 const args = process.argv.slice(2);
+
+// Store these values into variables
 const url = args[0];
 const filePath = args[1];
-let fileBody ='';
 
-console.log(args);
+// variables to set from the file to be downloaded
+let fileBody = '';
+let fileSize = 0;
 
+// Request the file from the url provided 
 request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  fileBody = String(body);
+  if (error !== null) {
+    console.log(error);
+  } else {
+    fileBody = String(body);
+    fileSize = fileBody.length;
 
-  fs.writeFile(filePath, fileBody, (error) => {
-    if (error) {
-      // Handle error
-      console.log("Failed to write to file");
-      return;
-    }
-    // Success!
-    console.log("Successfully wrote to file");
-  });
+    fs.writeFile(filePath, fileBody, (error) => {
+      if (error) {
+        // Handle error
+        console.log("Failed to write to file");
+        return;
+      }
+
+      // Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
+      console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
+    });
+  }
 });
 
-//t should download the resource at the URL to the local path on your machine
-
-// Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
-
-
-// I.e. node fetcher.js http://www.example.edu/ ./index.html
-// Downloaded and saved 3261 bytes to ./index.html
